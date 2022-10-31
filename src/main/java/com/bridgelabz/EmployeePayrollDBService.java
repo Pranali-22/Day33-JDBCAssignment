6711c2e636a2b3dc3196c28d2c24e8831651d052
Pranali-22 <lpranali22@gmail.com>
2022-10-31 22:13:30 +0530

    Added: UC2 - Read data from database

---
+++ src/main/java/com/bridgelabz/EmployeePayrollDBService.java
@@ -0,0 +1,148 @@
+package com.bridgelabz;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollDBService {
+
+    private PreparedStatement employeePayrollDataStatement;
+    private static EmployeePayrollDBService employeePayrollDBService;
+
+    public EmployeePayrollDBService() {
+    }
+
+    // Method -- class method -- one instance will be there only... if you call it one or more it will be same.
+    public static EmployeePayrollDBService getInstance(){
+        if (employeePayrollDBService == null)
+            employeePayrollDBService = new EmployeePayrollDBService();
+        return employeePayrollDBService;
+    }
+
+    private Connection getConnection() throws SQLException {
+        String jdbcUrl = "jdbc:mysql://127.0.0.7:3306/payroll_service?allowKeyRetrieval=true&useSSL=false";
+        // localhost = 127.0.0.1
+        String userName = "root";
+        String password = "Panu@2209";
+        Connection connection;
+
+        System.out.println("Connecting to database:"+jdbcUrl);
+        connection = DriverManager.getConnection(jdbcUrl,userName,password);
+        System.out.println("================================");
+        System.out.println("Connection is successfull"+connection);
+
+        return connection;
+    }
+
+   public List<EmployeePayrollData> readData(){
+
+        String sqlStatement = "SELECT id,empName,basicPay from employee_payroll;";
+        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlStatement);
+
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("empName");
+                double basicSalary = resultSet.getDouble("basicPay");
+                //LocalDate startDate = resultSet.getDate("startDate").toLocalDate();
+                employeePayrollDataList.add(new EmployeePayrollData(id,name,basicSalary,LocalDate.now()));
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return employeePayrollDataList;
+
+    }
+
+
+    public int updateEmployeeSalary(String name, double salary){
+
+        return this.updateEmployeeDataUsingStatement(name, salary);
+    }
+
+
+    //SQL injection
+    // Statement -- interface - calls DDL statments and execute it  - UC3
+    private int updateEmployeeDataUsingStatement(String name, double salary){
+        //String sql for normal statment.
+        String sqlStatement = String.format("Update employee_payroll set basicPay = %.2f where empName='%s';",salary,name);
+
+
+        try(Connection connection = getConnection()){
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sqlStatement);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+
+
+    // PepareStatment for UC4
+
+
+
+
+
+
+
+
+
+    // PepareStatment
+    private void prepareStatementForEmployeeData(){
+        try {
+            Connection connection = this.getConnection();
+            String sqlStatement = "Select * from employee_payroll where empName = ?;";
+            employeePayrollDataStatement = connection.prepareStatement(sqlStatement);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet){
+        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+
+        try {
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("empName");
+                double basicSalary = resultSet.getDouble("basicPay");
+                //LocalDate startDate = resultSet.getDate("startDate").toLocalDate();
+                employeePayrollDataList.add(new EmployeePayrollData(id,name,basicSalary,LocalDate.now()));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return employeePayrollDataList;
+    }
+
+
+    public List<EmployeePayrollData> getEmployeePayrollData(String name){
+        List<EmployeePayrollData> employeePayrollDataList = null;
+        if(this.employeePayrollDataStatement == null)
+            this.prepareStatementForEmployeeData();
+
+        try{
+            employeePayrollDataStatement.setString(1,name);
+            ResultSet resultSet = employeePayrollDataStatement.executeQuery();
+            employeePayrollDataList = this.getEmployeePayrollData(resultSet);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return  employeePayrollDataList;
+    }
+
+
+}
---
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,34 @@
+package com.bridgelabz;
+
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+	public int employeeId; // col
+	public String employeeName; // col
+	public double employeeSalary; //col
+	public LocalDate startDate; //col
+	
+	public EmployeePayrollData(Integer id, String name, Double salary, LocalDate date) {
+		
+		this.employeeId = id;
+		this.employeeName = name;
+		this.employeeSalary = salary;
+		this.startDate = date;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "EmployeeId: "+employeeId+", EmployeeName: "+employeeName+", EmployeeSalary: "+employeeSalary;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if(this == object)
+			return true;
+		if(object == null || getClass() != object.getClass())
+			return false;
+		EmployeePayrollData that = (EmployeePayrollData) object;
+		return employeeId == that.employeeId && Double.compare(that.employeeSalary,  employeeSalary) == 0 && employeeName.equals(that.employeeName);
+	}
+}
